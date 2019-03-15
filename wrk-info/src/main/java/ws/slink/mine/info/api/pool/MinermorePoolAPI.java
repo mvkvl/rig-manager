7330e635f655e42f13eecff908d9cdcb8092246d
Mikhail Kantur <kami@slink.ws>
2019-03-15 20:42:36 +1000

    MinerMore API fix

--- wrk-info/src/main/java/ws/slink/mine/info/api/pool/MinermorePoolAPI.java
+++ wrk-info/src/main/java/ws/slink/mine/info/api/pool/MinermorePoolAPI.java
@@ -61,7 +61,7 @@ public class MinermorePoolAPI implements PoolAPI {
                 .worker(minerInfo.worker)
                 .hashrate(workerHashrate / 1000000.0)
                 .average(averageWorkerHashrate / 1000000.0)
-                .confirmed(Double.parseDouble(balanceJson.getString("mature") + balanceJson.getString("balance")))
+                .confirmed(Double.parseDouble(balanceJson.getString("mature")) + Double.parseDouble(balanceJson.getString("balance")))
                 .unconfirmed(Double.parseDouble(balanceJson.getString("immature")));
     }
 
